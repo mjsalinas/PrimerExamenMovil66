8b2d5d84dd6b442884daa875849352dd87d78fe1
Heydi <heydialvarado15jya@gmail.com>
2026-08-22 14:16:25 -0600

    Commit init

--- components/AnswerButton.tsx
+++ components/AnswerButton.tsx
@@ -1,4 +1,5 @@
-import { Pressable, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
 
 type AnswerButtonProps = {
   label: string;
